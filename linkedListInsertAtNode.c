978ad4f7ba45de2e4118f2c23950cad737e80fae
Gurnoor Kaur <85249027+kgurnoor@users.noreply.github.com>
2024-07-15 21:47:46 +0530

    linked list insertion at node and insertion at the end.

---
+++ linkedListInsertAtNode.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+// inserting element at th(e end.
+struct Node *insertAtNode(struct Node *head, struct Node *prevNote, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = prevNote->next;
+    prevNote->next = ptr;
+    return head;
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = second;
+    second->data = 11;
+    second->next = third;
+    third->data = 41;
+    third->next = fourth;
+    fourth->data = 66;
+    fourth->next = NULL;
+
+    linkedListTraversal(head);
+    printf("\n");
+    head = insertAtNode(head, second, 32);
+    linkedListTraversal(head);
+    return 0;
+}
